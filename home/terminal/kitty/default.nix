2936d41826c6d322b990b14948395d2cf79dbe5b
asyncedd <isynqquwu@proton.me>
2024-01-13 12:14:57 +0900

    feat(kitty): Disable cursor blinking

--- home/terminal/kitty/default.nix
+++ home/terminal/kitty/default.nix
@@ -3,6 +3,9 @@
     enable = true;
     settings = {
       window_margin_width = "5";
+      enable_audio_bell = false;
+      update_check_interval = 0;
+      cursor_blink_interval = 0;
     };
     theme = "One Dark";
     shellIntegration.enableZshIntegration = true;
