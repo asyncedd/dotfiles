0c8b94609bacf0d5625cbee967ff07ff09d803a0
asyncedd <isynqquwu@proton.me>
2024-01-13 12:05:57 +0900

    feat: Install some packages

--- home/software/packages.nix
+++ home/software/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   home.packages = with pkgs; [
@@ -6,5 +6,16 @@
     lazygit
     feh
     keepassxc
+    obsidian
+    neovim
+    mpv
+  ];
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "obsidian"
+  ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
   ];
 }
