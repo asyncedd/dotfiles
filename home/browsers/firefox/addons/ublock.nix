10b43b5986740edeaa736edea681d51e9dc2907e
asyncedd <isynqquwu@proton.me>
2024-02-09 21:13:12 +0900

    perf(firefox): ignore genetic cosmetic lists

--- home/browsers/firefox/addons/ublock.nix
+++ home/browsers/firefox/addons/ublock.nix
@@ -7,6 +7,7 @@
     ];
     userSettings = [
       [ "advancedUserEnabled" "true" ]
+      [ "ignoreGeneticCosmeticFilters" "true" ]
       [ "popupPanelSections" "31" ]
     ];
     toOverwrite = {
