1b8860913d614f5d299437f06cd4a3c8f5607edf
asyncedd <isynqquwu@proton.me>
2024-02-12 12:09:45 +0900

    refactor(ubo): declare smoothscroll

--- home/browsers/firefox/default.nix
+++ home/browsers/firefox/default.nix
@@ -65,6 +65,56 @@
       @import "${inputs.lepton}/chrome/userContent.css";
       @import "${inputs.firefox-mod-blur}/userContent.css";
     '';
+    smoothScrolling = ''
+      user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS",   12);//NSS    [120]
+      user_pref("general.smoothScroll.msdPhysics.enabled",                    true);//NSS  [false]
+      user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",   200);//NSS   [1250]
+      user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",       250);//NSS   [1000]
+      user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",           25);//NSS     [12]
+      user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",     "2.0");//NSS    [1.3]
+      user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",      250);//NSS   [2000]
+      user_pref("general.smoothScroll.currentVelocityWeighting",             "1.0");//NSS ["0.25"]
+      user_pref("general.smoothScroll.stopDecelerationWeighting",            "1.0");//NSS  ["0.4"]
+      
+      /// adjust multiply factor for mousewheel - or set to false if scrolling is way too fast  
+      user_pref("mousewheel.system_scroll_override.horizontal.factor",         200);//NSS    [200]
+      user_pref("mousewheel.system_scroll_override.vertical.factor",           200);//NSS    [200]
+      user_pref("mousewheel.system_scroll_override_on_root_content.enabled",  true);//NSS   [true]
+      user_pref("mousewheel.system_scroll_override.enabled",                  true);//NSS   [true]
+      
+      /// adjust pixels at a time count for mousewheel - cant do more than a page at once if <100
+      user_pref("mousewheel.default.delta_multiplier_x",                       100);//NSS    [100]
+      user_pref("mousewheel.default.delta_multiplier_y",                       100);//NSS    [100]
+      user_pref("mousewheel.default.delta_multiplier_z",                       100);//NSS    [100]
+      
+      ///  this preset will reset couple extra variables for consistency
+      user_pref("apz.allow_zooming",                                          true);//NSS   [true]
+      user_pref("apz.force_disable_desktop_zooming_scrollbars",              false);//NSS  [false]
+      user_pref("apz.paint_skipping.enabled",                                 true);//NSS   [true]
+      user_pref("apz.windows.use_direct_manipulation",                        true);//NSS   [true]
+      user_pref("dom.event.wheel-deltaMode-lines.always-disabled",           false);//NSS  [false]
+      user_pref("general.smoothScroll.durationToIntervalRatio",                200);//NSS    [200]
+      user_pref("general.smoothScroll.lines.durationMaxMS",                    150);//NSS    [150]
+      user_pref("general.smoothScroll.lines.durationMinMS",                    150);//NSS    [150]
+      user_pref("general.smoothScroll.other.durationMaxMS",                    150);//NSS    [150]
+      user_pref("general.smoothScroll.other.durationMinMS",                    150);//NSS    [150]
+      user_pref("general.smoothScroll.pages.durationMaxMS",                    150);//NSS    [150]
+      user_pref("general.smoothScroll.pages.durationMinMS",                    150);//NSS    [150]
+      user_pref("general.smoothScroll.pixels.durationMaxMS",                   150);//NSS    [150]
+      user_pref("general.smoothScroll.pixels.durationMinMS",                   150);//NSS    [150]
+      user_pref("general.smoothScroll.scrollbars.durationMaxMS",               150);//NSS    [150]
+      user_pref("general.smoothScroll.scrollbars.durationMinMS",               150);//NSS    [150]
+      user_pref("general.smoothScroll.mouseWheel.durationMaxMS",               200);//NSS    [200]
+      user_pref("general.smoothScroll.mouseWheel.durationMinMS",                50);//NSS     [50]
+      user_pref("layers.async-pan-zoom.enabled",                              true);//NSS   [true]
+      user_pref("layout.css.scroll-behavior.spring-constant",                "250");//NSS    [250]
+      user_pref("mousewheel.transaction.timeout",                             1500);//NSS   [1500]
+      user_pref("mousewheel.acceleration.factor",                               10);//NSS     [10]
+      user_pref("mousewheel.acceleration.start",                                -1);//NSS     [-1]
+      user_pref("mousewheel.min_line_scroll_amount",                             5);//NSS      [5]
+      user_pref("toolkit.scrollbox.horizontalScrollDistance",                    5);//NSS      [5]
+      user_pref("toolkit.scrollbox.verticalScrollDistance",                      3);//NSS      [3]
+    '';
     search = {
       force = true;
       default = "Brave";
@@ -89,6 +139,7 @@
           (builtins.readFile "${inputs.betterfox}/user.js")
           (builtins.readFile "${inputs.lepton}/user.js")
           (builtins.readFile "${inputs.edge-frfox}/user.js")
+          smoothScrolling
           ''
             // Better fonts
             user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100);
@@ -106,48 +157,6 @@
 
            user_pref("network.trr.mode", 3);
            user_pref("network.trr.uri", "https://mozilla.cloudflare-dns.com/dns-query");
-
-           user_pref("apz.allow_zooming",                               true);            ///     true
-           user_pref("apz.force_disable_desktop_zooming_scrollbars",   false);            ///    false
-           user_pref("apz.paint_skipping.enabled",                      true);            ///     true
-           user_pref("apz.windows.use_direct_manipulation",             true);            ///     true
-           user_pref("dom.event.wheel-deltaMode-lines.always-disabled", true);            ///    false
-           user_pref("general.smoothScroll.currentVelocityWeighting", "0.12");            ///   "0.25" <- 1. If scroll too slow, set to "0.15"
-           user_pref("general.smoothScroll.durationToIntervalRatio",    1000);            ///      200
-           user_pref("general.smoothScroll.lines.durationMaxMS",         100);            ///      150
-           user_pref("general.smoothScroll.lines.durationMinMS",           0);            ///      150
-           user_pref("general.smoothScroll.mouseWheel.durationMaxMS",    100);            ///      200
-           user_pref("general.smoothScroll.mouseWheel.durationMinMS",      0);            ///       50
-           user_pref("general.smoothScroll.mouseWheel.migrationPercent", 100);            ///      100
-           user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);   ///      120
-           user_pref("general.smoothScroll.msdPhysics.enabled",                  true);   ///    false
-           user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 200);   ///     1250
-           user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",     200);   ///     1000
-           user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",         10);   ///       12
-           user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",  "1.20");   ///    "1.3"
-           user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",   1000);   ///     2000
-           user_pref("general.smoothScroll.other.durationMaxMS",         100);            ///      150
-           user_pref("general.smoothScroll.other.durationMinMS",           0);            ///      150
-           user_pref("general.smoothScroll.pages.durationMaxMS",         100);            ///      150
-           user_pref("general.smoothScroll.pages.durationMinMS",           0);            ///      150
-           user_pref("general.smoothScroll.pixels.durationMaxMS",        100);            ///      150
-           user_pref("general.smoothScroll.pixels.durationMinMS",          0);            ///      150
-           user_pref("general.smoothScroll.scrollbars.durationMaxMS",    100);            ///      150
-           user_pref("general.smoothScroll.scrollbars.durationMinMS",      0);            ///      150
-           user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6");            ///    "0.4"
-           user_pref("layers.async-pan-zoom.enabled",                   true);            ///     true
-           user_pref("layout.css.scroll-behavior.spring-constant",   "250.0");            ///   "250.0"
-           user_pref("mousewheel.acceleration.factor",                     3);            ///       10
-           user_pref("mousewheel.acceleration.start",                     -1);            ///       -1
-           user_pref("mousewheel.default.delta_multiplier_x",            100);            ///      100
-           user_pref("mousewheel.default.delta_multiplier_y",            100);            ///      100
-           user_pref("mousewheel.default.delta_multiplier_z",            100);            ///      100
-           user_pref("mousewheel.min_line_scroll_amount",                  0);            ///        5
-           user_pref("mousewheel.system_scroll_override.enabled",       false);            ///     true <- 2. If scroll too fast, set to false
-           user_pref("mousewheel.system_scroll_override_on_root_content.enabled", false); ///     true
-           user_pref("mousewheel.transaction.timeout",                  1500);            ///     1500
-           user_pref("toolkit.scrollbox.horizontalScrollDistance",         4);            ///        5
-           user_pref("toolkit.scrollbox.verticalScrollDistance",           3);            ///        3
         ''];
         inherit userChrome userContent;
       };
@@ -160,6 +169,7 @@
           (builtins.readFile "${inputs.betterfox}/user.js")
           (builtins.readFile "${inputs.lepton}/user.js")
           (builtins.readFile "${inputs.edge-frfox}/user.js")
+          smoothScrolling
           ''
             // Better fonts
             user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100);
@@ -178,47 +188,6 @@
            user_pref("network.trr.mode", 3);
            user_pref("network.trr.uri", "https://mozilla.cloudflare-dns.com/dns-query");
 
-           user_pref("apz.allow_zooming",                               true);            ///     true
-           user_pref("apz.force_disable_desktop_zooming_scrollbars",   false);            ///    false
-           user_pref("apz.paint_skipping.enabled",                      true);            ///     true
-           user_pref("apz.windows.use_direct_manipulation",             true);            ///     true
-           user_pref("dom.event.wheel-deltaMode-lines.always-disabled", true);            ///    false
-           user_pref("general.smoothScroll.currentVelocityWeighting", "0.12");            ///   "0.25" <- 1. If scroll too slow, set to "0.15"
-           user_pref("general.smoothScroll.durationToIntervalRatio",    1000);            ///      200
-           user_pref("general.smoothScroll.lines.durationMaxMS",         100);            ///      150
-           user_pref("general.smoothScroll.lines.durationMinMS",           0);            ///      150
-           user_pref("general.smoothScroll.mouseWheel.durationMaxMS",    100);            ///      200
-           user_pref("general.smoothScroll.mouseWheel.durationMinMS",      0);            ///       50
-           user_pref("general.smoothScroll.mouseWheel.migrationPercent", 100);            ///      100
-           user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);   ///      120
-           user_pref("general.smoothScroll.msdPhysics.enabled",                  true);   ///    false
-           user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 200);   ///     1250
-           user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",     200);   ///     1000
-           user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",         10);   ///       12
-           user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",  "1.20");   ///    "1.3"
-           user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",   1000);   ///     2000
-           user_pref("general.smoothScroll.other.durationMaxMS",         100);            ///      150
-           user_pref("general.smoothScroll.other.durationMinMS",           0);            ///      150
-           user_pref("general.smoothScroll.pages.durationMaxMS",         100);            ///      150
-           user_pref("general.smoothScroll.pages.durationMinMS",           0);            ///      150
-           user_pref("general.smoothScroll.pixels.durationMaxMS",        100);            ///      150
-           user_pref("general.smoothScroll.pixels.durationMinMS",          0);            ///      150
-           user_pref("general.smoothScroll.scrollbars.durationMaxMS",    100);            ///      150
-           user_pref("general.smoothScroll.scrollbars.durationMinMS",      0);            ///      150
-           user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6");            ///    "0.4"
-           user_pref("layers.async-pan-zoom.enabled",                   true);            ///     true
-           user_pref("layout.css.scroll-behavior.spring-constant",   "250.0");            ///   "250.0"
-           user_pref("mousewheel.acceleration.factor",                     3);            ///       10
-           user_pref("mousewheel.acceleration.start",                     -1);            ///       -1
-           user_pref("mousewheel.default.delta_multiplier_x",            100);            ///      100
-           user_pref("mousewheel.default.delta_multiplier_y",            100);            ///      100
-           user_pref("mousewheel.default.delta_multiplier_z",            100);            ///      100
-           user_pref("mousewheel.min_line_scroll_amount",                  0);            ///        5
-           user_pref("mousewheel.system_scroll_override.enabled",       false);            ///     true <- 2. If scroll too fast, set to false
-           user_pref("mousewheel.system_scroll_override_on_root_content.enabled", false); ///     true
-           user_pref("mousewheel.transaction.timeout",                  1500);            ///     1500
-           user_pref("toolkit.scrollbox.horizontalScrollDistance",         4);            ///        5
-           user_pref("toolkit.scrollbox.verticalScrollDistance",           3);            ///        3
         ''];
         inherit userChrome userContent;
       };
