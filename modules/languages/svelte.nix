04bb948afc01d7002da26b053e19e9a53fbd91dd
asyncedd <isynqquwu@proton.me>
2024-03-09 08:38:14 +0900

    feat(bun): init

--- modules/languages/svelte.nix
+++ modules/languages/svelte.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     nodePackages.svelte-language-server
     prettierd
     emmet-ls
     tailwindcss-language-server
+    bun
   ];
 }
