f5c166bb81d130cb1ecb15f2f3631b483d59419d
asyncedd <isynqquwu@proton.me>
2024-03-08 16:29:30 +0900

    feat: profile generation 992

--- home/browsers/firefox/addons/ublock.nix
+++ home/browsers/firefox/addons/ublock.nix
@@ -1,18 +1,16 @@
-{ lib, ... }:
-
-{
+{lib, ...}: {
   programs.firefox.policies."3rdparty".Extensions."uBlock0@raymondhill.net" = {
     adminSettings = {
       dynamicFilteringString = lib.concatMapStrings (x: x + "\n") [
         "behind-the-scene * * noop"
-	"behind-the-scene * inline-script noop"
+        "behind-the-scene * inline-script noop"
         "behind-the-scene * 1p-script noop"
-	"behind-the-scene * 3p-script noop"
-	"behind-the-scene * 3p-frame noop"
-	"behind-the-scene * image noop"
-	"behind-the-scene * 3p noop"
-	"* * 3p-frame block"
-	"* * 3p-script block"
+        "behind-the-scene * 3p-script noop"
+        "behind-the-scene * 3p-frame noop"
+        "behind-the-scene * image noop"
+        "behind-the-scene * 3p noop"
+        "* * 3p-frame block"
+        "* * 3p-script block"
 
         "duolingo.com * 3p-frame noop"
         "duolingo.com * 3p-script noop"
@@ -27,12 +25,16 @@
         "reddit.com * 3p-script noop"
         "youtube.com * 3p-frame noop"
         "youtube.com * 3p-script noop"
+        "*.spotify.com * 3p-script noop"
 
         "internals.rust-lang.org * 3p-script noop"
         "users.rust-lang.org * 3p-script noop"
         "community.brave.com * 3p-script noop"
 
+        "gemini.google.com * 3p-script noop"
+
         "* challenges.cloudflare.com 3p-script noop"
+        "* obsidian.md 3p-script allow"
         "* challenges.cloudflare.com 3p-frame noop"
       ];
       hostnameSwitchesString = lib.concatMapStrings (x: x + "\n") [
@@ -41,51 +43,51 @@
       ];
     };
     advancedSettings = [
-      [ "cnameMaxTTL" "720" ]
-      [ "filterAuthorMode" "true" ]
-      [ "updateAssetBypassBrowserCache" "true" ]
+      ["cnameMaxTTL" "720"]
+      ["filterAuthorMode" "true"]
+      ["updateAssetBypassBrowserCache" "true"]
     ];
     userSettings = [
-      [ "advancedUserEnabled" "true" ]
-      [ "ignoreGeneticCosmeticFilters" "true" ]
-      [ "popupPanelSections" "31" ]
+      ["advancedUserEnabled" "true"]
+      ["ignoreGeneticCosmeticFilters" "true"]
+      ["popupPanelSections" "31"]
     ];
     toOverwrite = {
       filterLists = [
-         "user-filters"
-         "ublock-filters"
-         "ublock-badware"
-         "ublock-privacy"
-         "ublock-quick-fixes"
-         "ublock-unbreak"
-         "block-lan"
-         "easyprivacy"
-         "urlhaus-1"
-         "plowe-0"
-         "ublock-annoyances"
-         "adguard-generic"
-         "adguard-spyware"
-         "adguard-spyware-url"
-         "adguard-popup-overlays"
-         "adguard-social"
-         "adguard-widgets"
-         "adguard-cookies"
-         "ublock-cookies-adguard"
-         "easylist-chat"
-         "easylist-newsletters"
-         "easylist-notifications"
-         "easylist-annoyances"
-         "fanboy-social"
-         "fanboy-cookiemonster"
-         "ublock-cookies-easylist"
-         "easylist"
-         "JPN-1"
-         "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
-         "https://raw.githubusercontent.com/yokoffing/filterlists/main/privacy_essentials.txt"
-         "https://raw.githubusercontent.com/yokoffing/filterlists/main/annoyance_list.txt"
-         "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt"
-         "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds-ublock.txt"
-         "https://big.oisd.nl/"
+        "user-filters"
+        "ublock-filters"
+        "ublock-badware"
+        "ublock-privacy"
+        "ublock-quick-fixes"
+        "ublock-unbreak"
+        "block-lan"
+        "easyprivacy"
+        "urlhaus-1"
+        "plowe-0"
+        "ublock-annoyances"
+        "adguard-generic"
+        "adguard-spyware"
+        "adguard-spyware-url"
+        "adguard-popup-overlays"
+        "adguard-social"
+        "adguard-widgets"
+        "adguard-cookies"
+        "ublock-cookies-adguard"
+        "easylist-chat"
+        "easylist-newsletters"
+        "easylist-notifications"
+        "easylist-annoyances"
+        "fanboy-social"
+        "fanboy-cookiemonster"
+        "ublock-cookies-easylist"
+        "easylist"
+        "JPN-1"
+        "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
+        "https://raw.githubusercontent.com/yokoffing/filterlists/main/privacy_essentials.txt"
+        "https://raw.githubusercontent.com/yokoffing/filterlists/main/annoyance_list.txt"
+        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt"
+        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds-ublock.txt"
+        "https://big.oisd.nl/"
       ];
       externalLists = "https://filters.adtidy.org/extension/ublock/filters/101_optimized.txt\nhttps://filters.adtidy.org/extension/ublock/filters/122_optimized.txt\nhttps://filters.adtidy.org/extension/ublock/filters/14_optimized.txt\nhttps://filters.adtidy.org/extension/ublock/filters/2_optimized.txt\nhttps://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt\nhttps://raw.githubusercontent.com/DandelionSprout/adfilt/master/Special%20security%20lists/AntiFaviconList.txt\nhttps://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt\nhttps://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds-ublock.txt\nhttps://raw.githubusercontent.com/yokoffing/filterlists/main/annoyance_list.txt\nhttps://raw.githubusercontent.com/yokoffing/filterlists/main/privacy_essentials.txt\nhttps://big.oisd.nl/";
       importedLists = [
@@ -98,16 +100,15 @@
         "https://big.oisd.nl/"
       ];
       trustedSiteDirectives = [
-         "about-scheme"
-         "chrome-extension-scheme"
-         "chrome-scheme"
-         "edge-scheme"
-         "moz-extension-scheme"
-         "opera-scheme"
-         "vivaldi-scheme"
-         "wyciwyg-scheme"
+        "about-scheme"
+        "chrome-extension-scheme"
+        "chrome-scheme"
+        "edge-scheme"
+        "moz-extension-scheme"
+        "opera-scheme"
+        "vivaldi-scheme"
+        "wyciwyg-scheme"
       ];
     };
   };
 }
-
