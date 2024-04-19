7eb7154668d8d8e83fe6368e5b2308f0fcfd7e0d
asyncedd <isynqquwu@proton.me>
2024-04-19 20:52:26 +0900

    fix(obsidian): fix it not using fcitx5

--- home/software/packages.nix
+++ home/software/packages.nix
@@ -13,6 +13,14 @@
     eza
     yazi
 
-    unstable.obsidian
+    (unstable.obsidian.overrideAttrs (e: rec {
+      # Add arguments to the .desktop entry
+      desktopItem = e.desktopItem.override (d: {
+        exec = "${d.exec} --enable-wayland-ime ";
+      });
+
+      # Update the install script to use the new .desktop entry
+      installPhase = builtins.replaceStrings ["${e.desktopItem}"] ["${desktopItem}"] e.installPhase;
+    }))
   ];
 }
