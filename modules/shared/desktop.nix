b7f42cf35ba65ece89ea267682a953a7ee0f6bd6
asyncedd <isynqquwu@proton.me>
2024-04-22 18:00:49 +0900

    fix: rm unneeded xserver stuff

--- modules/shared/desktop.nix
+++ modules/shared/desktop.nix
@@ -3,15 +3,6 @@
   inputs,
   ...
 }: {
-  services.xserver.enable = true;
-
-  # services.xserver.displayManager.sddm.enable = true;
-
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
   # Hyprland
   programs.hyprland = {
     enable = true;
