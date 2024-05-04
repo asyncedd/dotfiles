96d37c0031d4b653913469d8eb8d91ecb4baa74c
asyncedd <isynqquwu@proton.me>
2024-05-05 08:01:27 +0900

    feat(hyprlock): add bg

--- home/desktop/hyprlock.nix
+++ home/desktop/hyprlock.nix
@@ -1,5 +1,51 @@
 {
   programs.hyprlock = {
     enable = true;
+
+    general = {
+      disable_loading_bar = true;
+      hide_cursor = false;
+      no_fade_in = true;
+    };
+
+    backgrounds = [
+      {
+        monitor = "";
+        path = "${../../wallpapers/wallpaper-black-blur.jpg}";
+      }
+    ];
+
+    input-fields = [
+      {
+        monitor = "eDP-1";
+
+        size = {
+          width = 300;
+          height = 50;
+        };
+
+        outline_thickness = 2;
+
+        dots_spacing = 0.3;
+        dots_center = true;
+      }
+    ];
+
+    labels = [
+      {
+        monitor = "";
+        text = "$TIME";
+        font_family = "Lexend";
+        font_size = 50;
+
+        position = {
+          x = 0;
+          y = 80;
+        };
+
+        valign = "center";
+        halign = "center";
+      }
+    ];
   };
 }
