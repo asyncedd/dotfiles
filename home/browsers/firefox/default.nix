159a2358214e48cf70485d1c52729b150e4f5bbf
asyncedd <isynqquwu@proton.me>
2024-05-17 12:19:54 +0900

    feat(ff): use arc browser theme as the selected theme

--- home/browsers/firefox/default.nix
+++ home/browsers/firefox/default.nix
@@ -39,34 +39,23 @@ in {
         extraConfig = lib.strings.concatStrings [
           (builtins.readFile "${inputs.betterfox}/user.js")
           ''
-                             user_pref("browser.startup.page", 3); // 0102
-                             // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
-                             // user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
-                             user_pref("privacy.clearOnShutdown.history", false); // 2811
-                             // user_pref("privacy.cpd.history", false); // 2820 optional to match when you use Ctrl-Shift-Del
+            user_pref("browser.startup.page", 3); // 0102
+            // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
+            // user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
+            user_pref("privacy.clearOnShutdown.history", false); // 2811
+            // user_pref("privacy.cpd.history", false); // 2820 optional to match when you use Ctrl-Shift-Del
 
-                             user_pref("network.trr.mode", 3);
-                             user_pref("network.trr.uri", "https://dns.nextdns.io/5c65c8");
+            user_pref("network.trr.mode", 3);
+            user_pref("network.trr.uri", "https://dns.nextdns.io/5c65c8");
 
-                             user_pref("svg.context-properties.content.enabled", true);
-                        user_pref("gfx.webrender.all", true);
-                  user_pref("media.ffmpeg.vaapi.enabled", true);
-            user_pref("media.rdd-process.enabled", true);
+            user_pref("svg.context-properties.content.enabled", true);
+            user_pref("gfx.webrender.all", true);
+            user_pref("media.ffmpeg.vaapi.enabled", true);
+            user_pref("media.rdd-process.enabled", false);
+            user_pref("extensions.activeThemeID", "arc-dark-theme@afnankhan");
           ''
         ];
         inherit userChrome userContent;
-      };
-      profiles.arkenfox = {
-        id = 1;
-        name = "arkenfox";
-        isDefault = false;
-        inherit search;
-        extraConfig = lib.strings.concatStrings [
-          (builtins.readFile "${inputs.arkenfox}/user.js")
-          ''
-            user_pref("browser.formfill.enable", true);
-          ''
-        ];
       };
     };
   };
