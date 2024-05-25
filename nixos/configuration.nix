487cb750b6d532f32a0ac1b23cfaa298f7a34eff
asyncedd <isynqquwu@proton.me>
2024-05-25 14:27:42 +0900

    feat(nh): use gc

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -209,4 +209,11 @@
   # services.xserver.displayManager.startx.enable = true;
 
   hardware.opengl.enable = true;
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/user/.dotfiles";
+  };
 }
