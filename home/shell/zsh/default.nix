b11ccd9e1d82513b5b935c14681b12ffea8ea1e1
asyncedd <isynqquwu@proton.me>
2024-06-01 17:45:49 +0900

    perf(zsh): make zsh startuptimes BLAZINGLY FAST
    Prior to this commit, I didn't have powerline10k's instant prompt on.
    Now it is and it is, BLAZINGLY FAST
    
    fix(zsh): fix formatting

--- home/shell/zsh/default.nix
+++ home/shell/zsh/default.nix
@@ -28,6 +28,14 @@ in {
     # initExtraBeforeCompInit = ''
     #   fpath+=(${pkgs.zsh-completions}/share/zsh/site-functions)
     # '';
+    initExtraFirst = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
     initExtra = ''
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
         source ${./p10k.zsh}
@@ -87,8 +95,6 @@ in {
       	}
 
       source <(${pkgs.carapace}/bin/carapace _carapace)
-
-      ${pkgs.krabby}/bin/krabby random
     '';
     shellAliases = {
       ll = "ls -l";
