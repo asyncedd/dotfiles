1e1231063c16beb9d7b6bf02899cd5c0b99536b8
asyncedd <isynqquwu@proton.me>
2024-06-09 09:24:50 +0900

    perf: use tlp instead of cpu-autofreq

--- nixos/power.nix
+++ nixos/power.nix
@@ -1,30 +1,30 @@
 {
   lib,
-  inputs,
+  # inputs,
   ...
 }: {
   imports = [
-    inputs.auto-cpufreq.nixosModules.default
+    # inputs.auto-cpufreq.nixosModules.default
   ];
   services.system76-scheduler.settings.cfsProfiles.enable = true;
 
-  programs.auto-cpufreq = {
-    enable = true;
-    settings = {
-      charger = {
-        governor = "performance";
-        turbo = "auto";
-      };
-
-      battery = {
-        governor = "powersave";
-        turbo = "auto";
-      };
-    };
-  };
+  # programs.auto-cpufreq = {
+  #   enable = false;
+  #   settings = {
+  #     charger = {
+  #       governor = "performance";
+  #       turbo = "auto";
+  #     };
+  #
+  #     battery = {
+  #       governor = "powersave";
+  #       turbo = "auto";
+  #     };
+  #   };
+  # };
 
   services.power-profiles-daemon.enable = lib.mkForce false;
   powerManagement.powertop.enable = lib.mkForce true;
   services.thermald.enable = true;
-  services.tlp.enable = lib.mkForce false;
+  services.tlp.enable = true;
 }
