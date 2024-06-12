036fab2e6ace226fba6ca6de15743107740fe2f3
asyncedd <isynqquwu@proton.me>
2024-06-12 19:09:07 +0900

    fix: remove dearrow
    I'm removing dearrow as it makes the Youtube titles misalingned. It's
    not with the anti-clickbait titles, I swear. It's about the videos being
    displayed on feed and the actual (including anti-clickbait'ed) titles
    being different.

--- home/browsers/firefox/policies.nix
+++ home/browsers/firefox/policies.nix
@@ -51,7 +51,6 @@
         "https://addons.mozilla.org/firefox/downloads/file/3743190/ddg_bangs_but_faster-0.2.2.xpi" # DuckDuckGo !bangs but Faster
         "https://addons.mozilla.org/firefox/downloads/file/4208483/return_youtube_dislikes-3.0.0.14.xpi" # Return Youtube Dislikes
         "https://github.com/bpc-clone/bpc_updates/releases/download/latest/bypass_paywalls_clean-latest.xpi" # BPC
-        "https://addons.mozilla.org/firefox/downloads/file/4254118/dearrow-1.5.11.xpi" # dearrow
         "https://addons.mozilla.org/firefox/downloads/file/3920533/skip_redirect-2.3.6.xpi" # skip_redirect
       ];
     };
