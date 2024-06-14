c9d00c48b94f0f6c0fc1c84ba4f89f89394315f0
asyncedd <isynqquwu@proton.me>
2024-06-14 15:51:44 +0900

    fix(hyprland): rm unneeded keys

--- nixos/nix-daemon.nix
+++ nixos/nix-daemon.nix
@@ -16,14 +16,14 @@
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
         # "https://nyx.chaotic.cx"
-        "https://hyprland.cachix.org"
+        # "https://hyprland.cachix.org"
       ];
       extra-trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         # "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         # "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
